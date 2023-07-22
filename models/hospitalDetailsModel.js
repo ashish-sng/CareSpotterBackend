0c66383dc1d11a64a4d34563307dcaac18403748
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-23 01:16:08 +0530

    refactored

--- models/hospitalDetailsModel.js
+++ models/hospitalDetailsModel.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 
 const hospitalDetailsSchema = new mongoose.Schema({
-    hospitalName: {
-        type: String,
-        required: true,
-    },
-    hospitalAddress: {
-        type: String,
-        required: true,
-    },
-    latitude: {
-        type: Number,
-        required: true,
-    },
-    longitude: {
-        type: Number,
-        required: true,
-    },
-    area: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-    }
+  hospitalName: {
+    type: String,
+    required: true,
+  },
+  hospitalAddress: {
+    type: String,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
 });
 
-const hospitaldetails = mongoose.model("hospitaldetails", hospitalDetailsSchema);
+const hospitaldetails = mongoose.model(
+  "hospitaldetails",
+  hospitalDetailsSchema
+);
 
 module.exports = hospitaldetails;
