c52a7342bf9475ff948f9c11f4e3e9e9b3e12791
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-23 03:23:14 +0530

    tweaks

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    mongoose
-      .connect(
-        process.env.MONGODB_URL,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      )
-      .then(() => {
-        console.log("Connected to MongoDB");
-        console.log(`Server is running on port ${PORT}`);
-      })
-      .catch((err) => {
-        console.log("Error connecting to MongoDB", err);
-      });
-
-
-  
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+      console.log(`Server is running on port ${PORT}`);
+    })
+    .catch((err) => {
+      console.log("Error connecting to MongoDB", err);
+    });
 });
