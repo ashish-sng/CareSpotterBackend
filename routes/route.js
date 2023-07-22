d62af9015ea6eb82be3828780997b886e85335db
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-23 01:15:03 +0530

    added getUniqueArea route

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { getHospitals } = require("../controllers/hospitals");
-
+const express = require("express");
+const { getHospitals, getUniqueAreas } = require("../controllers/hospitals");
 
 const router = express.Router();
 
 router.route("/getHospitals").get(getHospitals);
+router.route("/getUniqueAreas").get(getUniqueAreas);
 
 module.exports = router;
